4bd61b4455e276a8e92e55ed152cd1c2b7f389b7
Fox5091 <70080711+Fox5091@users.noreply.github.com>
2024-06-14 23:54:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ app = Flask(__name__, template_folder='templates')
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 # Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 
 # Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 # OPENAI API Key初始化設定
 openai.api_key = os.getenv('OPENAI_API_KEY')
